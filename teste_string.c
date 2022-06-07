56eb08b5ed4a4261cfc7364e8ed1b76e314e1419
Roberth Juno <70485631+RoberthJuno@users.noreply.github.com>
2022-06-06 22:03:41 -0300

    Valor de elemento da string 2

---
+++ teste_string.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<conio.h>
+#include<locale.h>
+#include<string.h>
+
+int main( ){
+	setlocale(LC_ALL,"Portuguese");
+	system("color e");
+	
+	char nome[10] = "Djulian";
+	int i;
+	
+	for(i=0; i<7; i++){
+		printf("Valor do elemento %d da string = %c \n", i, nome[i]);
+	}
+	
+	//printf("Digite seu nome: ");
+	//printf("\n");
+	//gets(nome);
+	
+	printf("Nome: %s \n",nome);
+	
+	system("pause");
+	return 0;
+	
+}
