e847dafe8bf3912ae6df76d0ebde7435ad11f5f7
Roberth Juno <70485631+RoberthJuno@users.noreply.github.com>
2022-06-06 22:20:16 -0300

    Lista de letras

---
+++ lista de letras_for.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<locale.h>
+
+int main(){
+	
+	setlocale(LC_ALL,"Portuguese");
+	system("color D");
+	
+	char letra[5];  // 5 posições
+	int i;
+	
+	for (i=0; i<5;i++);
+		
+		printf("Digite a %dª letra: ",i+1);
+		fflush(stdin);  // limpar buffer do teclado
+		scanf("%c", &letra[i]);
+	}
+	printf("\n Lista das Letras: \n");
+	
+	for (i=0; i<5; i++){
+		
+		printf("%dª letra: %c2c \n",i+1,letra[i]);
+	}
+	printf("\n\n");
+	system("pause");
+	return 0;
+}
